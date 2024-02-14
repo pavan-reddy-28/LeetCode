7625957ff77bc3477035990eaaf7761bd3fd2688
Sai Pavan Reddy Pogula <35028241+pavan-reddy-28@users.noreply.github.com>
2024-02-13 21:42:07 -0800

    Time: 398 ms (5.00%), Space: 45.6 MB (15.25%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,57 @@
+class Solution {
+    public String minWindow(String s, String t) {
+         HashMap<Character,Integer> pattern = new HashMap<>();
+        
+        for(Character key : t.toCharArray()){
+            int value = pattern.getOrDefault(key,0);
+            pattern.put(key,value+1);
+        }
+
+        String resultString =s;
+        boolean visited=false;
+        int i=0;
+        int j=0;
+        int count = pattern.size();
+        char key =s.charAt(j);
+        int value =0;
+        while(i<=( s.length() - t.length())){
+            
+            while( count >0 &&  j<s.length()){
+                key = s.charAt(j);
+                value = pattern.getOrDefault(key,Integer.MIN_VALUE);
+                if(value!= Integer.MIN_VALUE){
+                    value--;
+                    pattern.put(key,value);
+                    if(value==0){
+                        count--;
+                    }
+                    System.out.println(" j : "+j+" key "+key);
+                }
+                j++;
+            }
+
+          if(count==0){
+                String result = s.substring(i,j);
+          visited=true;
+               
+                resultString = result.length() < resultString.length() ? result :resultString;
+                key = s.charAt(i);
+                value = pattern.getOrDefault(key,Integer.MIN_VALUE);
+          }
+                if(value != Integer.MIN_VALUE){
+                    value++;
+                    if(value ==1){
+                        count++;
+                    }
+                    pattern.put(key,value);
+                }
+
+            i++;
+
+
+
+        }
+        return visited?resultString:"";
+    
+    }
+}
